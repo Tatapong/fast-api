697967dc55a9aeb4a118dbb3608d969d15d0df84
Tatapong1 <info.salesvaccine@gmail.com>
2025-01-21 13:59:57 +0100

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,10 @@
+from fastapi.testclient import TestClient
+from app.main import app  # Ensure this correctly points to the FastAPI app instance
 
-# #from fastapi.testclient import TestClient
-# from app.main import app  # Ensure this correctly points to the FastAPI app instance
+client = TestClient(app)
 
-# client = TestClient(app)
+def test_read_main():
+    response = client.get("/")  # Ensure this is the correct endpoint you want to test
+    assert response.status_code == 200
+    assert response.json() == {"msg": "FIN DE LA DEMO DEVOPS"} 
 
-# def test_read_main():
-#     response = client.get("/")  # Ensure this is the correct endpoint you want to test
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "FIN DE LA DEMO DEVOPS"} 
-# **/
